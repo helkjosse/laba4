049c43521326079067a7546528c5e45fb793766f
helkjosse <joke9please@gmail.com>
2023-09-13 21:30:55 +0500

    Update and bug fix

--- main.py
+++ main.py
@@ -6,45 +6,26 @@ def calculate(staff_number: int, staff_home_distances: list, taxi_number_and_tar
         print("Ошибка входных данных")
         return
 
-    staff_home_distances.sort(reverse=True)
+    # Соединить каждого сотрудника с такси, отсортировать по расстоянию в порядке убывания
+    staff = sorted(list(enumerate(staff_home_distances, start=1)), key=lambda x: x[1], reverse=True)
 
-    sorted_taxi_number_and_tariff = {k: v for k, v in sorted(taxi_number_and_tariff.items(),
-                                                             key=lambda item: item[1])}
-    index = 0
+    # Сопоставить каждое такси с его тарифом, отсортировать по тарифам в порядке возрастания
+    taxis = sorted(list(taxi_number_and_tariff.items()), key=lambda x: x[1])
+
     summ = 0
+    assignments = [0] * staff_number
 
-    for elements in sorted_taxi_number_and_tariff.items():
-        print(elements[0])
-
-        summ += elements[1] * staff_home_distances[index]
-        index += 1
+    for i in range(staff_number):
+        employee, distance = staff[i]
+        taxi, tariff = taxis[i]
+        summ += distance * tariff
+        assignments[employee - 1] = taxi  # Назначить это такси сотруднику
+    # Вывести назначенное такси
+    for i in range(staff_number):
+        print(f'Работник № {i + 1} должен сесть в такси № {assignments[i]}')
 
     print(summ)
     print(convert.convert_number_to_words(summ))
-
-
-def test1():
-    print("Тест 1")
-    staff_n = 3
-    staff_h_d = [20, 40, 60]
-    taxi_n_t = {1: 30, 2: 10, 3: 20}
-    calculate(staff_n, staff_h_d, taxi_n_t)
-
-
-def test2():
-    print("Тест 2")
-    staff_n = 5
-    staff_h_d = [5, 10, 1, 15, 15]
-    taxi_n_t = {1: 3, 2: 3, 3: 3, 4: 2, 5: 3}
-    calculate(staff_n, staff_h_d, taxi_n_t)
-
-
-def test3():
-    print("Тест 3")
-    staff_n = 1
-    staff_h_d = []
-    taxi_n_t = {5: 130}
-    calculate(staff_n, staff_h_d, taxi_n_t)
 
 
 if __name__ == '__main__':
